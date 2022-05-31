ffa63bcc685af485679b995a8c1ca0b0a0ef1a8a
homuhomy <74242263+homuhomy@users.noreply.github.com>
2022-05-31 22:22:37 +0800

    Update Spam Checker

--- src/main/java/ds/confessionapp/adminPanel/SpamCheck.java
+++ src/main/java/ds/confessionapp/adminPanel/SpamCheck.java
@@ -1,88 +1,109 @@
-//package ds.confessionapp.adminPanel;
-//
-//import java.io.*;
-//import java.nio.file.Files;
-//import java.nio.file.Path;
-//import java.nio.file.Paths;
-//import java.util.List;
-//import java.util.Scanner;
-//
-//public class SpamCheck {
-//    //ok lets say there's a new confession submission, let store it temporarily as newPost.txt
-//    //newPost.txt will be compared with all the existing text files in the database.
-//    // if similarity >90%, either flagged or delete submission
-//    //if similariy <90%, the post will now have its own confession_Id (maybe check database last confession_id?) and will proceed to be saved in the database as usual
-//    //file will be in the queue
-//    private static double similarity;
-//
-//    public class ReadFile {
-//        public static String readFile(String filename) {
-//            StringBuffer sb = new StringBuffer();
-//            try {
-//                File file = new File(filename);
-//                Scanner input = new Scanner(file);
-//                while (input.hasNextLine()) {
-//                    sb.append(input.nextLine() + "\n");
-//                }
-//                input.close();
-//            } catch (Exception ex) {
-//                ex.printStackTrace();
-//            }
-//            return sb.toString();
-//        }
-//
-//    public static void main(String[] args) throws IOException {
-//
-//        String newCofPost = readFile("tempFiles/newPost.txt") ;
-//        System.out.println(newCofPost);
-//
-//        double idf = Math.log(1/1);
-//        System.out.println("informe o termo");
-//        Scanner in = new Scanner(System.in);
-//        String term = in.nextLine();
-//        System.out.println("informe o documento(arquivo/nome do arquivo)");
-//        String doc = in.nextLine();
-//    }
-//
-////        File newConfessionPost = new File("tempFiles/newPost.txt");
-////        InputStream inputStream = new FileInputStream(newConfessionPost);
-////
-////        Path dir = Paths.get("InputFiles");
-////
-////
-////        if(similarity == 0){
-////            Files.copy(inputStream, Paths.get("RetrievedFilesFromDB/"+ "getTheNewConfessionId"));
-////        }else{
-////            newConfessionPost.delete();
-////        }
-//    }
-//
-//        public class TFIDFCalculator {
-//            public double tf(List<String> doc, String term) {
-//                double result = 0;
-//                for (String word : doc) {
-//                    if (term.equalsIgnoreCase(word))
-//                        result++;
-//                }
-//                return result / doc.size();
-//            }
-//
-//            public double idf(List <List <String>> docs, String term) {
-//                double n = 0;
-//                for (List <String> doc : docs) {
-//                    for (String word : doc) {
-//                        if (term.equalsIgnoreCase(word)) {
-//                            n++;
-//                            break;
-//                        }
-//                    }
-//                }
-//                return Math.log(docs.size() / n);
-//            }
-//
-//            public double tfidf(List <String> doc, List <List <String>> docs, String term) {
-//                return tf(doc, term) * idf(docs, term);
-//            }
-//
-//
-//}
+package ds.confessionapp.adminPanel;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class SpamCheck {
+
+    //using Consine Similarity
+    private static class Values {
+
+        private int val1;
+        private int val2;
+
+        private Values(int v1, int v2) {
+            this.val1 = v1;
+            this.val2 = v2;
+        }
+
+        public void updateValues(int v1, int v2) {
+            this.val1 = v1;
+            this.val2 = v2;
+        }
+    }//end of class values
+
+    public double score(String text1, String text2) {
+        //1. Identify distinct words from both documents
+        String[] text1Words = text1.split(" ");
+        String[] text2Words = text2.split(" ");
+        Map<String, Values> wordFreqVector = new HashMap<>();
+        List<String> distinctWords = new ArrayList<>();
+
+        //prepare word frequency vector by using Text1
+        for (String text : text1Words) {
+            String word = text.trim();
+            if (!word.isEmpty()) {
+                if (wordFreqVector.containsKey(word)) {
+                    Values vals1 = wordFreqVector.get(word);
+                    int freq1 = vals1.val1 + 1;
+                    int freq2 = vals1.val2;
+                    vals1.updateValues(freq1, freq2);
+                    wordFreqVector.put(word, vals1);
+                } else {
+                    Values vals1 = new Values(1, 0);
+                    wordFreqVector.put(word, vals1);
+                    distinctWords.add(word);
+                }
+            }
+        }
+
+        //prepare word frequency vector by using Text2
+        for (String text : text2Words) {
+            String word = text.trim();
+            if (!word.isEmpty()) {
+                if (wordFreqVector.containsKey(word)) {
+                    Values vals1 = wordFreqVector.get(word);
+                    int freq1 = vals1.val1;
+                    int freq2 = vals1.val2 + 1;
+                    vals1.updateValues(freq1, freq2);
+                    wordFreqVector.put(word, vals1);
+                } else {
+                    Values vals1 = new Values(0, 1);
+                    wordFreqVector.put(word, vals1);
+                    distinctWords.add(word);
+                }
+            }
+        }
+
+        //calculate the cosine similarity score.
+        double vectAB = 0.0000000;
+        double vectA = 0.0000000;
+        double vectB = 0.0000000;
+        for (int i = 0; i < distinctWords.size(); i++) {
+            Values vals12 = wordFreqVector.get(distinctWords.get(i));
+            double freq1 = vals12.val1;
+            double freq2 = vals12.val2;
+            //System.out.println(distinctWords.get(i) + "#" + freq1 + "#" + freq2);
+            vectAB = vectAB + freq1 * freq2;
+            vectA = vectA + freq1 * freq1;
+            vectB = vectB + freq2 * freq2;
+        }
+
+        //System.out.println("VectAB " + vectAB + " VectA_Sq " + vectA + " VectB_Sq " + vectB);
+        return ((vectAB) / (Math.sqrt(vectA) * Math.sqrt(vectB)));
+    }
+
+    public static void main(String[] args) throws IOException {
+        SpamCheck cs = new SpamCheck();
+
+        String text1 = Files.readAllLines(Paths.get("tempFiles/newPost.txt")).stream().collect(Collectors.joining(" "));
+        String text2 = Files.readAllLines(Paths.get("InputFiles/#UM011422.txt")).stream().collect(Collectors.joining(" "));
+
+        double score = cs.score(text1, text2);
+        System.out.println("Cosine similarity score = " + score);
+
+        if(score < 0.90){
+            //file will be saved to InputFiles
+        }else{
+            //delete file / masuk bin
+        }
+
+    }
+
+}
